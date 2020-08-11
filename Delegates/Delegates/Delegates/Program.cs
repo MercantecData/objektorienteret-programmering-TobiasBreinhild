27aa5af54c32a6b65d55f7f31efc82a8cc36ade5
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2020-08-11 09:02:14 +0200

    Added Delegates Project and Refleksion Document

---
+++ Delegates/Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void DelegateStuff();
+
+        static void Main(string[] args)
+        {
+            DelegateStuff stuff = StuffStuff;
+
+            stuff();
+        }
+
+        public static void StuffStuff()
+        {
+            Console.WriteLine("Even More Stuff!");
+        }
+    }
+}
